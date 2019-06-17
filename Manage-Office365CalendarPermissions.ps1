3fca68816db6a3947a26035cbd22b701a267d2c9
Simon Brown <simon.brown@bridgepartners.co.uk>
2019-06-17 18:50:20 +0100

    Initial Commit

---
+++ Manage-Office365CalendarPermissions.ps1
@@ -0,0 +1,46 @@
+Function Add-Office365CalendarPermissions {
+    [cmdletbinding(SupportsShouldProcess)]
+    Param (
+        [Parameter(Mandatory)][ValidateScript({
+            Get-Mailbox $_
+        })][String]$CalendarOwner,
+        [Parameter(Mandatory)][ValidateScript({
+            Get-Mailbox $_
+        })][String]$User,
+        [Parameter(Mandatory)][ValidateSet(
+            "Author",
+            "Contributor",
+            "Editor",
+            "None",
+            "NonEditingAuthor",
+            "Owner",
+            "PublishingEditor",
+            "PublishingAuthor",
+            "Reviewer",
+            "AvailabilityOnly",
+            "LimitedDetails"
+        )][String]$AccessRights
+    )
+    Get-Mailbox $CalendarOwner | Foreach-Object {
+        IF ($PSCmdlet.ShouldProcess($CalendarOwner)) {
+            Add-MailboxFolderPermission "$_`:Calendar" -User $User -AccessRights $AccessRights
+        }
+    }
+}
+
+Function Remove-Office365CalendarPermissions {
+    [cmdletbinding(SupportsShouldProcess)]
+    Param (
+        [Parameter(Mandatory)][ValidateScript({
+            Get-Mailbox $_
+        })][String]$CalendarOwner,
+        [Parameter(Mandatory)][ValidateScript({
+            Get-Mailbox $_
+        })][String]$User
+    )
+    Get-Mailbox $CalendarOwner | ForEach-Object {
+        IF ($PSCmdlet.ShouldProcess($CalendarOwner)) {
+            Remove-MailboxFolderPermission "$_`:Calendar" -User $User
+        }
+    }
+}
